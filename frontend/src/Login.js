7d0cc4f4616c2ec18d502aec520db5e375f5788d
marleekitchen <marlee.kitchen@gmail.com>
2023-03-06 23:18:06 -0800

    Create frontend design for login page

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,7 +1,23 @@
+import React, { useState } from "react";
+
 export const Login = (props) => {
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("tbd");
+    }
+
     return (
         <div className="form">
-            <button className='link-button' onClick={() => props.onFormSwitch('register')}>Create Account</button>
+            <h2>ready to grind? log in.</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+            <input value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="username" id="username" name="username" />
+            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="password" id="password" name="password" />
+            <button type="submit">log in</button>
+            </form>
+            <button className='link-button' onClick={() => props.onFormSwitch('register')}>create account</button>
         </div>
     )
 }
