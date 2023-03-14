b35af17cd7bed9dd91cc8adc6a821ae8cf1f9d58
Yuzhou-Gao <yuzhougao@g.ucla.edu>
2023-03-13 22:49:21 -0700

    Add a count feature to record the remainders number

--- frontend/src/remainderlist.js
+++ frontend/src/remainderlist.js
@@ -1,18 +1,23 @@
 import React from 'react';
 
 function RemainderList(props) {
+  
   const { items, title } = props;
 
+  const count = items.length;
+
   return (
-    <div className="todolist">
+    <div className="remainderlist">
+
           <h1>{title}</h1>
+
           <ul className="list-group list-group-flush">
 
           {items.map(item => (
               <li key={item.id} className="list-group-item">
                 <div className="form-check">
-                  <input className="form-check-input" type="checkbox" value="" id={`todo-item-check-${item.id}`} />
-                  <label className="form-check-label" htmlFor={`todo-item-check-${item.id}`}>
+                  <input className="form-check-input" type="checkbox" value="" id={`tremainder-item-check-${item.id}`} />
+                  <label className="form-check-label" htmlFor={`remainder-item-check-${item.id}`}>
                     {item.text}
                   </label>
                 </div>
@@ -20,6 +25,12 @@ function RemainderList(props) {
             ))}
 
           </ul>
+
+          <div className="remainder-footer">
+            <span className="count-remainders">{count}</span>
+            {' tasks left'}
+          </div>
+
         </div>
   );
 }
