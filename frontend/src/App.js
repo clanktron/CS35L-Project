7d6da425a2c9ed70e28efabf700ce03d3af1100a
Yuzhou-Gao <yuzhougao@g.ucla.edu>
2023-03-13 22:30:04 -0700

    Encapsulation of the RemaiderList object

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import RemainderList from './remainderlist';
 
 function App() {
   let items = [
@@ -18,23 +19,7 @@ function App() {
   return (
     <div className="container">
       <div className="row">
-        <div className="todolist">
-          <h1>{title}</h1>
-          <ul className="list-group list-group-flush">
-
-          {items.map(item => (
-              <li key={item.id} className="list-group-item">
-                <div className="form-check">
-                  <input className="form-check-input" type="checkbox" value="" id={`todo-item-check-${item.id}`} />
-                  <label className="form-check-label" htmlFor={`todo-item-check-${item.id}`}>
-                    {item.text}
-                  </label>
-                </div>
-              </li>
-            ))}
-
-          </ul>
-        </div>
+        <RemainderList title={title} items={items} />
       </div>
     </div>
   );
---
+++ frontend/src/remainderlist.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function RemainderList(props) {
+  const { items, title } = props;
+
+  return (
+    <div className="todolist">
+          <h1>{title}</h1>
+          <ul className="list-group list-group-flush">
+
+          {items.map(item => (
+              <li key={item.id} className="list-group-item">
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" value="" id={`todo-item-check-${item.id}`} />
+                  <label className="form-check-label" htmlFor={`todo-item-check-${item.id}`}>
+                    {item.text}
+                  </label>
+                </div>
+              </li>
+            ))}
+
+          </ul>
+        </div>
+  );
+}
+
+export default RemainderList;
