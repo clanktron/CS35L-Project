669928d88b84ecdfa5d87c27560650359e58cc66
marleekitchen <marlee.kitchen@gmail.com>
2023-03-14 20:49:24 -0700

    Update login page to communicate with backend via axios and ignore roachdata in .gitignore

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,21 +1,65 @@
-import React, { useState } from "react";
+import React, { useRef, useState, useEffect, useContext } from "react";
+import AuthContext from "./context/AuthProvider";
+import axios from "./api/axios"
 
 export const Login = (props) => {
+    const { setAuth } = useContext(AuthContext);
+    const userRef = useRef();
+    const errRef = useRef();
     const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
+    const [errMsg, setErrMsg] = useState("");
+    const [success, setSuccess] = useState(false);
+    useEffect(() => {
+      userRef.current.focus();
+    }, []);
+    useEffect(() => {
+      setErrMsg("");
+    }, [userName, password]);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log("tbd");
-    }
+        try {
+          const response = await axios.post(
+            'http://localhost:4000/login',
+            JSON.stringify({ userName, password }),
+            {
+              headers: {
+                cookie: 'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzgxOTEwMTMsInVzZXJpZCI6Ijg0NTc5NzgyODg4MDU5Njk5MyJ9.ib23dJ-KYCps9Bw589bA-ExjkouH1EmPfUCR5_KKiw8',
+                "Content-Type": "application/json"
+            },
+            data: {Username: 'admin', Password: 'admin'},
+              withCredentials: true,
+            }
+          );
+          const accessToken = response?.data?.accessToken;
+          const roles = response?.data?.roles;
+          setAuth({ userName, password, roles, accessToken });
+          setUserName("");
+          setPassword("");
+          setSuccess(true);
+        } catch (err) {
+          if (!err?.response) {
+            setErrMsg("No Server Response");
+          } else if (err.response?.status === 400) {
+            setErrMsg("Missing Username or Password");
+          } else if (err.response?.status === 401) {
+            setErrMsg("Unauthorized");
+          } else {
+            setErrMsg("Login Failed");
+          }
+          errRef.current.focus();
+        }
+      };
 
     return (
         <div className="form">
             <h2>ready to grind? log in.</h2>
+            <div> {!success && <>ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive" {errMsg}</>}</div>
             <form className="login-form" onSubmit={handleSubmit}>
-            <input value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="username" id="username" name="username" />
+            <input value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="username" id="username" name="username" ref={userRef}/>
             <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="password" id="password" name="password" />
-            <button type="submit">log in</button>
+            <button type="submit" onClick={() => props.onFormSwitch('mainpage')}>log in</button>
             </form>
             <button className='link-button' onClick={() => props.onFormSwitch('register')}>create account</button>
         </div>
---
+++ frontend/src/Mainpage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Mainpage = (props) => {    
+    return (
+        <>Mainpage</>
+    )
+}
