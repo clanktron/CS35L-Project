ce79dc44e7cf5df6c1b67bffb82adbc60b54d41f
LeothePold <leo.naddell@gmail.com>
2023-03-16 15:35:16 -0700

    sidebar changes

--- frontend/thesidebar/src/Pages/Home.js
+++ frontend/thesidebar/src/Pages/Home.js
@@ -1,42 +1,28 @@
 import React, { useState, useEffect } from 'react'
 
 export default function Home() {
-  const [username, setUsername] = useState([])
-  const [password, setPassword] = useState([])
+  const [username, password] = useState([])
 
-  const login = async (username, password) => {                 //fetch data by login credentials
-    await fetch('http://localhost:4000/login', {
-       method: 'POST',
-       mode: 'cors',
-       credentials: 'include',
-       headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-       },
-       body: JSON.stringify({
-          username: username,
-          password: password,
-       }),
-    })
-       .then((data) => console.log(data));
- };
- 
- const handleSubmit = (e) => {
-    e.preventDefault();
-    login(username, password);
- }; 
-  
   useEffect(() => {
-    fetch('http://localhost:4000') //fetch data from API, need help on this
-      .then(response => response.json()) // parse into json file
-      .then(response => console.log(response))
-      .then(data => password(data))
-      .catch(error => console.error(error)) //If there is an error, console.error function, which will log the error message to the console.
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:4000/list', {
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'include',
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+      const data = await response.json()
+      const names = data.map(item => item.name)
+      password(names)
+    }
+    fetchData().catch(error => console.error(error))
   }, [])
 
   return (
     <ul>
-      {username.map(item => <li key={item.id}>{item.name}</li>)}     
+      {username.map((name, index) => <li key={index}>{name}</li>)}     
     </ul>
-  ) //unordered list (<ul>) with each item represented by a list item
+  )
 }
-//list output
