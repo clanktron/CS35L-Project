c430456c9cf3d522f37acda18552234d09efb7cb
LeothePold <leo.naddell@gmail.com>
2023-03-16 13:02:42 -0700

    sidebar backend updates

--- frontend/thesidebar/src/Pages/Home.js
+++ frontend/thesidebar/src/Pages/Home.js
@@ -1,19 +1,42 @@
 import React, { useState, useEffect } from 'react'
 
 export default function Home() {
-  const [homeItems, setHomeItems] = useState([])
+  const [username, setUsername] = useState([])
+  const [password, setPassword] = useState([])
 
+  const login = async (username, password) => {                 //fetch data by login credentials
+    await fetch('http://localhost:4000/login', {
+       method: 'POST',
+       mode: 'cors',
+       credentials: 'include',
+       headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+       },
+       body: JSON.stringify({
+          username: username,
+          password: password,
+       }),
+    })
+       .then((data) => console.log(data));
+ };
+ 
+ const handleSubmit = (e) => {
+    e.preventDefault();
+    login(username, password);
+ }; 
+  
   useEffect(() => {
-    fetch('https://www.google.com/') //fetch data from API, need help on this
+    fetch('http://localhost:4000') //fetch data from API, need help on this
       .then(response => response.json()) // parse into json file
-      .then(data => setHomeItems(data))
-      .catch(error => console.error(error))
+      .then(response => console.log(response))
+      .then(data => password(data))
+      .catch(error => console.error(error)) //If there is an error, console.error function, which will log the error message to the console.
   }, [])
 
   return (
     <ul>
-      {homeItems.map(item => <li key={item.id}>{item.name}</li>)}     
+      {username.map(item => <li key={item.id}>{item.name}</li>)}     
     </ul>
-  )
+  ) //unordered list (<ul>) with each item represented by a list item
 }
 //list output
