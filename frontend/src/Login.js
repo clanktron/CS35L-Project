e78ae769edb5d34e134ca70275423d01a663821e
marleekitchen <marlee.kitchen@gmail.com>
2023-03-17 14:11:13 -0700

    Update login page to respond to user and backend

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,10 +1,25 @@
-import React, { useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 export const Login = (props) => {
+
+  const userRef = useRef();
+  const errRef = useRef();
+
   const [userName, setUserName] = useState('');
+
   const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState();
-  const errorMessageStyle = document.getElementById("error-msg");
+
+  const [errMsg, setErrMsg] = useState('');
+  const [success, setSuccess] = useState(false);
+  //const errorMessageStyle = document.getElementById("error-msg");
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, [])
+
+  useEffect(() => {
+    setErrMsg('');
+  }, [userName, password])
 
   const response = async (userName, password) => {
     await fetch('http://localhost:4000/login', {
@@ -21,28 +36,61 @@ export const Login = (props) => {
     })
     .then((response) => response.text())
     .then((text) => {
-      if (text === '') {
-        setErrorMessage(true);
-      }
-      else {
-        setErrorMessage(false);
-      }
-    })
-  };
+        // console.log(text);
+        if (text !== '') {
+            setSuccess(true);
+        } else {
+            setSuccess(false);
+            setErrMsg('incorrect username or password');
+            errRef.current.focus();
+        }
+    });
+};
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    response(userName, password);
+    /*response(userName, password);
     if (errorMessage) {
       errorMessageStyle.style.opacity = 1;
     }
     else {
       errorMessageStyle.style.opacity = 0;
       props.onFormSwitch('mainpage');
+    }*/
+    try {
+      response(userName, password);
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg('No Server Response');
+      } else {
+        setErrMsg('Log in Failed')
+      }
+      errRef.current.focus();
     }
-  };
+  }
 
   return (
+    <>
+        {success ? (props.onFormSwitch('mainpage')
+        ) : (
+          <div className="form">
+                <h2>ready to grind? log in.</h2>
+                <form className="login-form" onSubmit={handleSubmit}>
+                <input value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="username" 
+                id="username" name="username" ref={userRef} autoComplete="off"/>
+                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="password" 
+                id="password" name="password"/>
+                <button type="submit">log in</button>
+                </form>
+                <button className='link-button' onClick={() => props.onFormSwitch('register')}>create account</button>
+                <p ref={errRef} className={errMsg ? "err-msg" : "offscreen"}>{errMsg}</p>
+                </div>
+        )}
+    </>
+)
+}
+
+/*
       <div className="form">
           <h2>ready to grind? log in.</h2>
           <form className="login-form" onSubmit={handleSubmit}>
@@ -53,5 +101,4 @@ export const Login = (props) => {
           <button className='link-button' onClick={() => props.onFormSwitch('register')}>create account</button>
           <p id="error-msg">incorrect username or password</p>
       </div>
-  )
-}
+*/
