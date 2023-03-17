8145233e317d9b74824413dae7080e4e580d5cd5
marleekitchen <marlee.kitchen@gmail.com>
2023-03-17 13:32:20 -0700

    Tentative updates to login page

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -3,10 +3,10 @@ import React, { useState } from "react";
 export const Login = (props) => {
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const errRef = useRef();
+  const [errorMessage, setErrorMessage] = useState();
+  const errorMessageStyle = document.getElementById("error-msg");
 
-  const login = async (userName, password) => {
+  const response = async (userName, password) => {
     await fetch('http://localhost:4000/login', {
       method: 'POST',
       mode: 'cors',
@@ -19,54 +19,39 @@ export const Login = (props) => {
         password: password,
       }),
     })
-    .then((data) => console.log(data))
-    .catch(err => { 
-      if (!err?.response) {
-        setErrMsg('No Server Response');
-      } else if (err.response?.status === 400) {
-        setErrMsg('Missing Username or Password');
-      } else if (err.response?.status === 401) {
-        setErrMsg('Unauthorized');
-      } else {
-        setErrMsg('Login Failed');
+    .then((response) => response.text())
+    .then((text) => {
+      if (text === '') {
+        setErrorMessage(true);
       }
-      errRef.current.focus();
+      else {
+        setErrorMessage(false);
+      }
     })
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    login(userName, password);
+    response(userName, password);
+    if (errorMessage) {
+      errorMessageStyle.style.opacity = 1;
+    }
+    else {
+      errorMessageStyle.style.opacity = 0;
+      props.onFormSwitch('mainpage');
+    }
   };
 
   return (
       <div className="form">
           <h2>ready to grind? log in.</h2>
-          { ( error &&
-          <h3 className="error">"log in failed</h3> )
-          || ( props.onFormSwitch('mainpage') ) }
           <form className="login-form" onSubmit={handleSubmit}>
           <input value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="username" id="username" name="username" required/>
           <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="password" id="password" name="password" required/>
           <button type="submit" onSubmit={handleSubmit}>log in</button>
           </form>
           <button className='link-button' onClick={() => props.onFormSwitch('register')}>create account</button>
+          <p id="error-msg">incorrect username or password</p>
       </div>
   )
 }
-
-/*
-<error className="error" onSubmit={error ? <error value={error} /> : props.onFormSwitch('mainpage') }/>
-
-          {onSubmit={ if (error) { <error value={error} /> } else: { props:onFormSwitch('mainpage') }}}
-<button type="submit" onClick={() => props.onFormSwitch('mainpage')}>log in</button>
-
-          .then(handleErrors)
-    .then((data) => {
-      console.log(data)
-      props.onFormSwitch('mainpage')
-    }, reason => {
-      console.error(reason);
-      setErrorMessage('login failed')
-    });
-      */
