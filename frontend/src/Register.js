5f05d2363ef7c56598600eed7416b5dc54dee635
Daniel Wang <danielwang@Daniels-Galaxy-Book>
2023-03-17 13:56:56 -0700

    Merge login_and_register branch with mainpage branch locally, and have the basic functionality working, Styling to be done.

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -1,33 +1,246 @@
-import React, {useState} from 'react';
+import { useRef, useState, useEffect } from "react";
+// run npm i @fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons @fortawesome/react-fontawesome
+import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+
+
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
 
 export const Register = (props) => {
 
-    const [userName, setUserName] = useState('');
-    const [password, setPassword] = useState('');
-    const [cPassword, setCPassword] = useState('');
 
-    const handleSubmit = (e) => {
+    const userRef = useRef();
+    const errRef = useRef();
+
+
+    const [user, setUser] = useState('');
+    const [validName, setValidName] = useState(false);
+    const [userFocus, setUserFocus] = useState(false);
+
+
+    const [pwd, setPwd] = useState('');
+    const [validPwd, setValidPwd] = useState(false);
+    const [pwdFocus, setPwdFocus] = useState(false);
+
+
+    const [matchPwd, setMatchPwd] = useState('');
+    const [validMatch, setValidMatch] = useState(false);
+    const [matchFocus, setMatchFocus] = useState(false);
+
+
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+
+
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+
+    useEffect(() => {
+        setValidName(USER_REGEX.test(user));
+    }, [user])
+
+
+    useEffect(() => {
+        setValidPwd(PWD_REGEX.test(pwd));
+        setValidMatch(pwd === matchPwd);
+    }, [pwd, matchPwd])
+
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, pwd, matchPwd])
+
+
+
+
+
+
+    const response = async (user, pwd) => {
+        await fetch('http://localhost:4000/user', {
+            method: 'POST',
+            mode: 'cors',
+            credentials: 'include',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+            body: JSON.stringify({
+                username: user,
+                password: pwd,
+            }),
+        })
+        .then((response) => response.text())
+        .then((text) => {
+            // console.log(text);
+            if (text !== '') {
+                setSuccess(true);
+                setUser('');
+                setPwd('');
+                setMatchPwd('');
+            } else {
+                setSuccess(false);
+                setErrMsg('Username Taken');
+                errRef.current.focus();
+            }
+        });
+    };
+
+
+
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        if (password !== cPassword) {
-            console.log("password not the same");
-        } else {
-            console.log("success");
+        // if button enabled with JS hack
+        const v1 = USER_REGEX.test(user);
+        const v2 = PWD_REGEX.test(pwd);
+        if (!v1 || !v2) {
+            setErrMsg("Invalid Entry");
+            return;
+        }
+
+
+        try {
+            response(user, pwd);
+        } catch (err) {
+            if (!err?.response) {
+                setErrMsg('No Server Response');
+            } else {
+                setErrMsg('Registration Failed')
+            }
+            errRef.current.focus();
         }
     }
 
-    
+
+
+
+
+
+
+
     return (
-        <div className='form'>
-            <form className='register-form' onSubmit={handleSubmit}>
-                <label for="username">Username</label>
-                <input value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="Username" id="username" name="username" />
-                <label for="password">Password</label>
-                <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" id="password" name="password" />
-                <label for="cPassword">Confirm Password</label>
-                <input value={cPassword} onChange={(e) => setCPassword(e.target.value)} placeholder="Confirm Password" id="cPassword" name="cPassword" />
-                <button type='submit'>Create</button>
-            </form>
-            <button className='link-button' onClick={() => props.onFormSwitch('login')}>Back to Login</button>        
-        </div>
+        <>
+            {success ? (props.onFormSwitch('mainpage')
+                // <section>
+                //     <h1>Success!</h1>
+                //     <p>
+                //         <a href="#">Sign In</a>
+                //     </p>
+                // </section>
+            ) : (
+                <section>
+                    <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+                    <h1>Register</h1>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="username">
+                            Username:
+                            <FontAwesomeIcon icon={faCheck} className={validName ? "valid" : "hide"} />
+                            <FontAwesomeIcon icon={faTimes} className={validName || !user ? "hide" : "invalid"} />
+                        </label>
+                        <input
+                            type="text"
+                            id="username"
+                            ref={userRef}
+                            autoComplete="off"
+                            onChange={(e) => setUser(e.target.value)}
+                            value={user}
+                            required
+                            aria-invalid={validName ? "false" : "true"}
+                            aria-describedby="uidnote"
+                            onFocus={() => setUserFocus(true)}
+                            onBlur={() => setUserFocus(false)}
+                        />
+                        <p id="uidnote" className={userFocus && user && !validName ? "instructions" : "offscreen"}>
+                            <FontAwesomeIcon icon={faInfoCircle} />
+                            4 to 24 characters.<br />
+                            Must begin with a letter.<br />
+                            Letters, numbers, underscores, hyphens allowed.
+                        </p>
+
+
+
+
+                        <label htmlFor="password">
+                            Password:
+                            <FontAwesomeIcon icon={faCheck} className={validPwd ? "valid" : "hide"} />
+                            <FontAwesomeIcon icon={faTimes} className={validPwd || !pwd ? "hide" : "invalid"} />
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            onChange={(e) => setPwd(e.target.value)}
+                            value={pwd}
+                            required
+                            aria-invalid={validPwd ? "false" : "true"}
+                            aria-describedby="pwdnote"
+                            onFocus={() => setPwdFocus(true)}
+                            onBlur={() => setPwdFocus(false)}
+                        />
+                        <p id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
+                            <FontAwesomeIcon icon={faInfoCircle} />
+                            8 to 24 characters.<br />
+                            Must include uppercase and lowercase letters, a number and a special character.<br />
+                            Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
+                        </p>
+
+
+
+
+                        <label htmlFor="confirm_pwd">
+                            Confirm Password:
+                            <FontAwesomeIcon icon={faCheck} className={validMatch && matchPwd ? "valid" : "hide"} />
+                            <FontAwesomeIcon icon={faTimes} className={validMatch || !matchPwd ? "hide" : "invalid"} />
+                        </label>
+                        <input
+                            type="password"
+                            id="confirm_pwd"
+                            onChange={(e) => setMatchPwd(e.target.value)}
+                            value={matchPwd}
+                            required
+                            aria-invalid={validMatch ? "false" : "true"}
+                            aria-describedby="confirmnote"
+                            onFocus={() => setMatchFocus(true)}
+                            onBlur={() => setMatchFocus(false)}
+                        />
+                        <p id="confirmnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"}>
+                            <FontAwesomeIcon icon={faInfoCircle} />
+                            Must match the first password input field.
+                        </p>
+
+
+
+
+                        <button disabled={!validName || !validPwd || !validMatch ? true : false}>Sign Up</button>
+                    </form>
+                    <p>
+                        Already registered?<br />
+                        <button className='link-button' onClick={() => props.onFormSwitch('login')}>Back to Login</button>
+                    </p>
+                </section>
+            )}
+        </>
     )
 }
+
+
+
+// <div className='form'>
+// <form className='register-form' onSubmit={handleSubmit}>
+//     <label for="username">Username</label>
+//     <input value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="Username" id="username" name="username" />
+//     <label for="password">Password</label>
+//     <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" id="password" name="password" />
+//     <label for="cPassword">Confirm Password</label>
+//     <input value={cPassword} onChange={(e) => setCPassword(e.target.value)} placeholder="Confirm Password" id="cPassword" name="cPassword" />
+//     <button type='submit'>Create</button>
+// </form>
+// <button className='link-button' onClick={() => props.onFormSwitch('login')}>Back to Login</button>        
+// </div>
+
